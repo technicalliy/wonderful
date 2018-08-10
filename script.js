b427a3dc0e6a85fa2dcbfca84bfd94c322e45428
Alliy Frauenpreis <anf46@drexel.edu>
2018-08-09 23:17:34 -0700

    fade effect on scroll

--- script.js
+++ script.js
@@ -2,5 +2,14 @@
 $(document).ready( function (){
 
 
+	var height = 995;
+	$(window).scroll(function(){
+		// console.log($(window).scrollTop());
+		$('.logo-2').css("opacity", $(window).scrollTop()/height);
+		$('.logo-1').css("opacity", 1-$(window).scrollTop()/height);
+
+	});
+
+
 	console.log("YAY");
 });

83bce5b61408cd2f1d2cc5085bdb4a692f28196e
Alliy Frauenpreis <anf46@drexel.edu>
2018-08-10 14:31:27 -0700

    Last touches

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 
 $(document).ready( function (){
 
-
 	var height = 995;
+
 	$(window).scroll(function(){
-		// console.log($(window).scrollTop());
 		$('.logo-2').css("opacity", $(window).scrollTop()/height);
 		$('.logo-1').css("opacity", 1-$(window).scrollTop()/height);
+		$('#phone').css("opacity", $(window).scrollTop()/height);
+
+
+		$('#upper-left-cluster').css("opacity", 1-$(window).scrollTop()/(height/2));
+		$('#top-cluster').css("opacity", 1-$(window).scrollTop()/(height/2));
+		$('.window-logo').css("opacity", 1-$(window).scrollTop()/(height));
+
+		$('#top-center').css("opacity", $(window).scrollTop()/height);
 
 	});
 
-
-	console.log("YAY");
 });
